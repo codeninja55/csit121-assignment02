f905d68478ee3177a7e47620c13859343f8fbdfe
Andrew Che <andrew@codeninja55.me>
2017-08-25 00:17:15 +1000

    Added Temp Database class to store data

---
+++ com.cn55.program/src/cn55/model/Database.java
@@ -0,0 +1,18 @@
+package cn55.model;
+
+/* THIS CLASS IS A TEMP DATABASE */
+
+/*
+*
+* Things to move here:
+* - Cards ArrayList
+* - Purchases ArrayList
+* - Categories ArrayList
+* - ReceiptID Set
+* - CardID Set
+*
+* */
+
+public class Database {
+    
+}
