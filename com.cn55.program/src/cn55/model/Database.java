18148d128bae57614c6e776bf51674e7b73bc49e
Andrew Che <andrew@codeninja55.me>
2017-08-25 11:22:22 +1000

    Added accessors and mutators

--- com.cn55.program/src/cn55/model/Database.java
+++ com.cn55.program/src/cn55/model/Database.java
@@ -13,24 +13,63 @@ package cn55.model;
 *
 * */
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
-class Database {
+public class Database {
 
     private ArrayList<Card> cards;
     private ArrayList<Purchase> purchases;
-    private ArrayList<String> categories;
+    private Map<String, Double> categories;
     private Set<Integer> receiptSet;
     private Set<String> cardIDSet;
 
-    public Database() {
+    Database() {
         cards = new ArrayList<>();
         purchases = new ArrayList<>();
-        categories = new ArrayList<>();
+        categories = new HashMap<>();
         receiptSet = new HashSet<>();
         cardIDSet = new HashSet<>();
+    }
 
+    /*==================== MUTATORS ====================*/
+    public void addCards(Card card) {
+        this.cards.add(card);
+    }
+
+    public void addPurchases(Purchase purchase) {
+        this.purchases.add(purchase);
+    }
+
+    public void addCategory(String category, Double value) {
+        this.categories.put(category, value);
+    }
+
+    public void addReceipt(int receiptID) {
+        this.receiptSet.add(receiptID);
+    }
+
+    public void addCardID(String cardID) {
+        this.cardIDSet.add(cardID);
+    }
+
+    /*==================== ACCESSORS ====================*/
+    public ArrayList<Card> getCards() {
+        return cards;
+    }
+
+    public ArrayList<Purchase> getPurchases() {
+        return purchases;
+    }
+
+    public Map<String, Double> getCategories() {
+        return categories;
+    }
+
+    public Set<Integer> getReceiptSet() {
+        return receiptSet;
+    }
+
+    public Set<String> getCardIDSet() {
+        return cardIDSet;
     }
 }
