4592864cd8164bcc888a9d915820de8741b5f4e9
Andrew Che <andrew@codeninja55.me>
2017-08-27 00:39:06 +1000

    Added Event classes and corresponding Listeners

--- com.cn55.program/src/cn55/view/CardEvent.java
+++ com.cn55.program/src/cn55/view/CardEvent.java
@@ -2,15 +2,15 @@ package cn55.view;
 
 import java.util.EventObject;
 
-public class PurchaseEvent extends EventObject {
+public class CardEvent extends EventObject {
 
     private String cardID;
 
-    public PurchaseEvent(Object source) {
+    public CardEvent(Object source) {
         super(source);
     }
 
-    public PurchaseEvent(Object source, String cardID) {
+    public CardEvent(Object source, String cardID) {
         super(source);
         this.cardID = cardID;
     }
---
+++ com.cn55.program/src/cn55/view/PurchaseListener.java
@@ -0,0 +1,5 @@
+package cn55.view;
+
+public interface PurchaseListener {
+    void formActionOccurred(PurchaseEvent event);
+}
