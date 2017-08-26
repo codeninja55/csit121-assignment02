cad532213414aeb7a5deebbab0e4528aebb5535f
Andrew Che <andrew@codeninja55.me>
2017-08-26 15:31:06 +1000

    Added PurchaseToolbar to panel

--- com.cn55.program/src/cn55/view/PurchasesPanel.java
+++ com.cn55.program/src/cn55/view/PurchasesPanel.java
@@ -1,15 +1,19 @@
 package cn55.view;
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 
 public class PurchasesPanel extends JPanel {
     private JPanel purchasesPanel;
+    private PurchaseToolbar toolbar;
 
     public PurchasesPanel() {
         purchasesPanel = new JPanel();
+        toolbar = new PurchaseToolbar();
 
         setLayout(new BorderLayout());
 
+        add(toolbar, BorderLayout.NORTH);
         add(purchasesPanel, BorderLayout.CENTER);
 
     }
