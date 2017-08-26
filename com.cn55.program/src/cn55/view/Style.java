90d2b9a5f1bd7f2163a631e092bedd98e13c873c
Andrew Che <andrew@codeninja55.me>
2017-08-26 13:13:15 +1000

    Added a Style class to modify UI look

---
+++ com.cn55.program/src/cn55/view/Style.java
@@ -0,0 +1,16 @@
+package cn55.view;
+
+import java.awt.*;
+
+public class Style {
+    public static Font buttonFont() { return new Font("Verdana", Font.BOLD, 24); }
+    public static Font labelFont() { return new Font("Verdana", Font.BOLD, 24); }
+    public static Font titledBorderFont() { return new Font("Verdana", Font.BOLD, 24); }
+    public static Font textFieldFont() { return new Font("Monospaced", Font.PLAIN, 20); }
+    public static Font tabPaneFont() { return  new Font("Monospaced", Font.BOLD, 34); }
+
+    public static Color btnColor() { return new Color(244,67,54); }
+    public static Color red500() { return new Color(244,67,54); }
+    public static Color blueGrey500 () { return new Color(96,125,139); }
+    public static Color btnTextColor() { return new Color(245,245,245); }
+}
