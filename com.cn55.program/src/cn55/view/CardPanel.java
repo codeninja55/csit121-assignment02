f095e51cce11040e79052c97a69aa84ab81608ea
Andrew Che <andrew@codeninja55.me>
2017-08-26 13:25:46 +1000

    Implemented the table back into the panel for now

--- com.cn55.program/src/cn55/view/CardPanel.java
+++ com.cn55.program/src/cn55/view/CardPanel.java
@@ -1,16 +1,18 @@
 package cn55.view;
 
-import cn55.model.Card;
+import cn55.model.*;
 
 import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
+import javax.swing.table.AbstractTableModel;
 
 class CardPanel extends JPanel {
 
     private CardsToolbar toolbar;
     private SearchPanel searchPanel;
     private JTextArea cardTextArea;
+    private CardTableModel cardTableModel;
     private ArrayList<Card> cards;
 
     // Constructor
@@ -48,13 +50,89 @@ class CardPanel extends JPanel {
 
     void setSearchListener(SearchListener listener) { searchPanel.setSearchListener(listener); }
 
-    void setCardData(ArrayList<Card> cards) { this.cards = cards; }
+    /*void setCardData(ArrayList<Card> cards) { this.cards = cards; }*/
 
-    void refresh() {
+    public void setCardData(ArrayList<Card> cards) {
+        cardTableModel.setData(cards);
+    }
+
+    /*void refresh() {
         cardTextArea.setText(null);
         cardTextArea.setVisible(true);
         for (Card card : cards) {
             cardTextArea.append(card.toString());
+        }
+    }*/
+
+    public void refresh() {
+        cardTableModel.fireTableDataChanged();
+    }
+
+    // INNER CLASS
+    public class CardTableModel extends AbstractTableModel {
+
+        private ArrayList<Card> cards;
+        private String[] cardHeaders = {"Card ID", "Card Type", "Name", "Email", "Balance", "Points"};
+
+        public void setData(ArrayList<Card> cards) {
+            this.cards = cards;
+        }
+
+        @Override
+        public String getColumnName(int column) {
+            return cardHeaders[column];
+        }
+
+        @Override
+        public int getRowCount() {
+            return cards.size();
+        }
+
+        @Override
+        public int getColumnCount() {
+            return cardHeaders.length;
+        }
+
+        @Override
+        public Object getValueAt(int rowIndex, int columnIndex) {
+            Card card = cards.get(rowIndex);
+
+            switch (columnIndex) {
+                case 0:
+                    return card.getID();
+                case 1:
+                    return card.getCardType();
+                case 2:
+                    if (card instanceof BasicCard) {
+                        return ((BasicCard) card).getName();
+                    } else if (card instanceof PremiumCard) {
+                        return ((PremiumCard) card).getName();
+                    } else {
+                        return "";
+                    }
+
+                case 3:
+                    if (card instanceof BasicCard) {
+                        return ((BasicCard) card).getEmail();
+                    } else if (card instanceof PremiumCard) {
+                        return ((PremiumCard) card).getEmail();
+                    } else {
+                        return "";
+                    }
+
+                case 4:
+                    if (card instanceof BasicCard) {
+                        return ((BasicCard) card).getBalance();
+                    } else if (card instanceof PremiumCard) {
+                        return ((PremiumCard) card).getBalance();
+                    } else {
+                        return 0;
+                    }
+
+                case 5:
+                    return card.getPoints();
+            }
+            return null;
         }
     }
 }
