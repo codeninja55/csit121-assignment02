f875d976ef10efeb61a70db1c751235e942a762e
Andrew Che <andrew@codeninja55.me>
2017-08-29 11:05:00 +1000

    Moved Style class into CustomComponents package

--- com.cn55.program/src/cn55/view/CustomComponents/Style.java
+++ com.cn55.program/src/cn55/view/CustomComponents/Style.java
@@ -1,13 +1,14 @@
-package cn55.view;
+package cn55.view.CustomComponents;
 
 import java.awt.*;
 
 public class Style {
     public static Font buttonFont() { return new Font("Verdana", Font.BOLD, 24); }
-    public static Font labelFont() { return new Font("Verdana", Font.BOLD, 20); }
+    public static Font labelFont() { return new Font("Monospaced", Font.BOLD, 18); }
+    public static Font comboboxFont() { return new Font("Verdana", Font.PLAIN, 20); }
     public static Font titledBorderFont() { return new Font("Verdana", Font.BOLD, 24); }
-    public static Font textFieldFont() { return new Font("Monospaced", Font.PLAIN, 20); }
-    public static Font tabPaneFont() { return  new Font("Monospaced", Font.BOLD, 34); }
+    public static Font textFieldFont() { return new Font("Monospaced", Font.PLAIN, 18); }
+    public static Font tabPaneFont() { return  new Font("Verdana", Font.BOLD, 34); }
     public static Font tableDataFont() { return new Font("Monospaced", Font.PLAIN, 18); }
 
     public static Color btnColor() { return new Color(244,67,54); }
