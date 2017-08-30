1230365093d1feb601b9b43fc4117631639fac25
Andrew Che <andrew@codeninja55.me>
2017-08-30 14:59:19 +1000

    Partially completed logic for create purchase and button handler

--- com.cn55.program/src/cn55/controller/Program.java
+++ com.cn55.program/src/cn55/controller/Program.java
@@ -4,6 +4,9 @@ import cn55.model.*;
 import cn55.view.*;
 import cn55.model.CardModel.*;
 import cn55.view.CardView.*;
+import cn55.view.CustomComponents.FormLabel;
+import cn55.view.CustomComponents.FormTextField;
+import cn55.view.CustomComponents.ToolbarButton;
 import cn55.view.PurchaseView.*;
 import cn55.view.SearchPanel.*;
 
@@ -17,32 +20,33 @@ class Program {
     private Database db;
     private MainFrame mainFrame;
     private Shop shop;
-
-    /* ASK MARK SIFER ABOUT THIS */
     private JTabbedPane tabPane;
     private CardPanel cardPanel;
+    private CardsToolbar cardToolbar;
+    private CardForm cardForm;
     private PurchasesPanel purchasePanel;
-    private PurchaseForm purchaseForm;
     private PurchaseToolbar purchaseToolbar;
+    private PurchaseForm purchaseForm;
 
     Program() {
-
         shop = new Shop();
         db = shop.getDatabase();
         createTestCode(shop);
         db.mapCards();
 
-        this.mainFrame = new MainFrame(new ArrayList<>(shop.getDatabase().getPurchases()),
-                new ArrayList<>(shop.getDatabase().getCards()));
+        this.mainFrame = new MainFrame(shop.getDatabase().getPurchases(), shop.getDatabase().getCards());
 
         this.tabPane = mainFrame.getTabPane();
 
         this.cardPanel = mainFrame.getCardPanel();
+        this.cardToolbar = cardPanel.getCardToolbar();
+        this.cardForm = cardPanel.getCardForm();
 
         this.purchasePanel = mainFrame.getPurchasePanel();
         this.purchaseForm = purchasePanel.getPurchaseForm();
         this.purchaseToolbar = purchasePanel.getPurchaseToolbar();
 
+        System.out.println();
         eventControlling();
     }
 
@@ -58,7 +62,7 @@ class Program {
         cat1.put("Fashion", 0D);
         cat1.put("Deals", 500D);
 
-        shop.makePurchase("cash", cat1);
+        shop.makePurchase("Cash", shop.generateReceiptID(), cat1);
 
         // AnonCard Test
         db.addCards(new AnonCard("111"));
@@ -71,7 +75,7 @@ class Program {
         cat2.put("Toys", 100D);
         cat2.put("Motors", 0D);
 
-        shop.makePurchase("111", cat2);
+        shop.makePurchase("111",shop.generateReceiptID(), cat2);
 
         db.addCards(new AnonCard("112"));
 
@@ -83,7 +87,7 @@ class Program {
         cat8.put("Toys", 0D);
         cat8.put("Motors", 0D);
 
-        shop.makePurchase("112", cat8);
+        shop.makePurchase("112",shop.generateReceiptID(), cat8);
 
         // BasicCard Test
         db.addCards(new BasicCard("69", "Natasha Romanov",
@@ -97,7 +101,7 @@ class Program {
         cat3.put("Toys", 1000D);
         cat3.put("Deals", 2000D);
 
-        shop.makePurchase("69", cat3);
+        shop.makePurchase("69",shop.generateReceiptID(), cat3);
 
         // BasicCard Test 2
         db.addCards(new BasicCard("001", "Steve Rogers",
@@ -111,7 +115,7 @@ class Program {
         cat4.put("Toys", 100D);
         cat4.put("Deals", 2000D);
 
-        shop.makePurchase("001", cat4);
+        shop.makePurchase("001",shop.generateReceiptID(), cat4);
 
         // PremiumCard Test
         db.addCards(new PremiumCard("75", "Tony Stark",
@@ -125,7 +129,7 @@ class Program {
         cat5.put("Other", 500D);
         cat5.put("Fashion", 2000D);
 
-        shop.makePurchase("75", cat5);
+        shop.makePurchase("75",shop.generateReceiptID(), cat5);
 
         // PremiumCard Test 2
         db.addCards(new PremiumCard("666", "Nick Fury",
@@ -139,7 +143,7 @@ class Program {
         cat6.put("Other", 500D);
         cat6.put("Fashion", 2000D);
 
-        shop.makePurchase("666", cat6);
+        shop.makePurchase("666",shop.generateReceiptID(), cat6);
 
         db.addCards(new AnonCard("113"));
         db.addCards(new AnonCard("114"));
@@ -159,34 +163,34 @@ class Program {
 
             /* SELECTED LISTENERS */
             if (tabPane.getSelectedComponent() == cardPanel) {
-                cardPanel.refresh();
+                cardPanel.refresh(db.getCards());
             } else if (tabPane.getSelectedComponent() == purchasePanel) {
                 purchaseToolbar.disableCreatePurchaseButton(false);
-                purchasePanel.refresh();
+                purchasePanel.refresh(shop.getDatabase().getPurchases());
             }
 
             /* DESELECTED LISTENERS */
             if (tabPane.getSelectedComponent() != purchasePanel) {
                 //IF PANEL CHANGED AWAY - REMOVE THE FORM
-                if (purchaseForm.getCreatePurchaseFormPanel() != null) {
-                    purchaseForm.getPurchaseTypeCombo().setSelectedIndex(0);
-                    purchaseForm.setVisible(false);
-                    purchaseForm.remove(purchaseForm.getCreatePurchaseFormPanel());
+                if (purchaseForm.getCreatePurchaseForm() != null) {
+                    removeCreatePurchaseForm();
                 }
             }
         });
 
-        /* TOOLBAR LISTENERS */
-        cardPanel.setCreateCardListener(new CardListener() {
-            public void formActionOccurred() {
-                CardForm createCardForm = new CardForm(shop.generateCardID());
+        /* CARD VIEW TOOLBAR LISTENERS */
+        cardToolbar.setCreateCardListener(new ToolbarButtonListener() {
+            public void toolbarButtonEventOccurred() {
+                /*CardForm createCardForm = new CardForm(shop.generateCardID());
                 HashMap<String,String> newCard = createCardForm.getCardMap();
                 shop.makeCard(newCard);
-                mainFrame.getCardPanel().refresh();
+                mainFrame.getCardPanel().refresh();*/
+
+
             }
         });
 
-        cardPanel.setDeleteCardListener(new CardListener() {
+        /*cardToolbar.setDeleteCardListener(new ToolbarButton() {
             public void formActionOccurred() {
                 CardForm deleteCardForm = new CardForm();
 
@@ -202,9 +206,9 @@ class Program {
                     cardPanel.refresh();
                 }
             }
-        });
+        });*/
 
-        cardPanel.setSearchListener(new SearchListener() {
+        /*cardToolbar.setSearchListener(new SearchListener() {
             public void searchEventOccurred(SearchEvent e) {
                 String cardID = e.getSearchID();
 
@@ -226,9 +230,9 @@ class Program {
                             JOptionPane.ERROR_MESSAGE);
                 }
             }
-        });
+        });*/
 
-        /* LISTENER FOR ADD PURCHASE BUTTON ON PURCHASE PANEL TOOLBAR */
+        /* PURCHASE VIEW TOOLBAR LISTENERS */
         purchaseToolbar.setCreatePurchaseListener(new ToolbarButtonListener() {
             public void toolbarButtonEventOccurred() {
                 purchaseToolbar.disableCreatePurchaseButton(true);
@@ -236,27 +240,88 @@ class Program {
                 purchaseForm.setGeneratedReceiptID(shop.generateReceiptID());
                 purchaseForm.setCardModel(shop.getDatabase().getCardModel());
                 purchaseForm.setCategoriesList(shop.getDatabase().getCategoriesList());
-                purchaseForm.setGeneratedCardID(shop.generateCardID());
 
-                purchaseForm.createPurchaseFormPanel();
+                purchaseForm.createPurchaseForm();
                 purchaseForm.setVisible(true);
             }
         });
 
-        /* LISTENER FOR CANCEL BUTTON IN PURCHASE FORM */
+        /* PURCHASE FORM CANCEL LISTENER */
         purchaseForm.setCancelPurchaseListener(new ButtonListener() {
             public void buttonActionOccurred() {
-                purchaseForm.getPurchaseTypeCombo().setSelectedIndex(0);
-                purchaseForm.setVisible(false);
-                purchaseForm.remove(purchaseForm.getCreatePurchaseFormPanel());
-                purchasePanel.getPurchaseToolbar().disableCreatePurchaseButton(false);
+                removeCreatePurchaseForm();
             }
         });
 
+        /* PURCHASE FORM CREATE LISTENER */
         purchaseForm.setCreatePurchaseListener(new PurchaseListener() {
-            @Override
             public void formActionOccurred(PurchaseEvent event) {
+                JComboBox<String> type = event.getPurchaseTypeCombo();
 
+                String receiptIDStr = event.getReceiptIDTextField().getText();
+                int receiptID = Integer.parseInt(receiptIDStr);
+
+                // TODO - Validation required
+                String cardType = "";
+                String name = "";
+                String email = "";
+                if (event.getAnonCardRB().isSelected()) {
+                    cardType = CardType.AnonCard.getName();
+                } else if (event.getBasicCardRB().isSelected()) {
+                    cardType = CardType.BasicCard.getName();
+                    name = event.getCardNameTextField().getText();
+                    email = event.getCardEmailTextField().getText();
+                } else if (event.getPremiumCardRB().isSelected()) {
+                    cardType = CardType.PremiumCard.getName();
+                    name = event.getCardNameTextField().getText();
+                    email = event.getCardEmailTextField().getText();
+                }
+
+                /* CATEGORIES */
+                // TODO - Validation required
+                Map<String, Double> categories = new HashMap<>();
+                for (HashMap.Entry<FormLabel, FormTextField> item : event.getCategoriesMap().entrySet()) {
+                    String labelStr = item.getKey().getText();
+                    String catName = labelStr.substring(0, labelStr.indexOf(":"));
+                    Double catValue = 0D;
+                    String textFieldStr = item.getValue().getText();
+                    if (!(textFieldStr.isEmpty()))
+                        catValue = Double.parseDouble(textFieldStr);
+
+                    categories.put(catName, catValue);
+                }
+
+                // TODO - Validation required if existing card used
+                // TODO - Validation required if new card created
+                String cardID;
+                if (type.getSelectedItem() != null) {
+                    if (type.getSelectedItem().equals(PurchaseType.ExistingCardPurchase.getName())) {
+                        cardID = (String)event.getExistingCardCombo().getSelectedItem();
+
+                        shop.makePurchase(cardID, receiptID, categories);
+                        purchasePanel.refresh(shop.getDatabase().getPurchases());
+                    } else if (type.getSelectedItem().equals(PurchaseType.NewCardPurchase.getName())) {
+                        cardID = event.getCardIDTextField().getText();
+
+                        HashMap<String, String> newCard = new HashMap<>();
+                        newCard.put("name", name);
+                        newCard.put("email",email);
+                        newCard.put("cardID", cardID);
+                        newCard.put("cardType",cardType);
+
+                        shop.makeCard(newCard);
+                        cardPanel.refresh(db.getCards());
+                        shop.makePurchase(cardID, receiptID, categories);
+                        purchasePanel.refresh(shop.getDatabase().getPurchases());
+                    } else if (type.getSelectedItem().equals(PurchaseType.CashPurchase.getName())) {
+                        cardID = CardType.Cash.getName();
+
+                        shop.makePurchase(cardID, receiptID, categories);
+                        purchasePanel.refresh(shop.getDatabase().getPurchases());
+                    }
+                }
+
+                removeCreatePurchaseForm();
             }
         });
     }
@@ -265,7 +330,7 @@ class Program {
         /* REMOVE THE FORM FROM PURCHASE */
         purchaseForm.getPurchaseTypeCombo().setSelectedIndex(0);
         purchaseForm.setVisible(false);
-        purchaseForm.remove(purchaseForm.getCreatePurchaseFormPanel());
+        purchaseForm.remove(purchaseForm.getCreatePurchaseForm());
         purchasePanel.getPurchaseToolbar().disableCreatePurchaseButton(false);
     }
 }
