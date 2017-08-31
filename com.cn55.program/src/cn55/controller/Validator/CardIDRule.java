215b6f91c6ebd93c1a03783bd8098929ff3b6891
Andrew Che <andrew@codeninja55.me>
2017-08-31 23:37:27 +1000

    Changed rule to inclde an "MC" at the front of Card ID

--- com.cn55.program/src/cn55/controller/Validator/CardIDRule.java
+++ com.cn55.program/src/cn55/controller/Validator/CardIDRule.java
@@ -3,7 +3,7 @@ package cn55.controller.Validator;
 import java.util.regex.Pattern;
 
 public class CardIDRule implements FormRule {
-    private static final String ID_PATTERN = "\\d*";
+    private static final String ID_PATTERN = "MC\\d*";
     private static final Pattern regex = Pattern.compile(ID_PATTERN);
 
     public boolean validate(FormValidData validData) {
