2c3a8a7fe9a93c55ce56ff684a4ceba61660ae9f
Andrew Che <andrew@codeninja55.me>
2017-08-31 17:09:47 +1000

    Added validator for Category Amounts from input

--- com.cn55.program/src/cn55/controller/Validator/CategoryAmountRule.java
+++ com.cn55.program/src/cn55/controller/Validator/CategoryAmountRule.java
@@ -1,11 +1,13 @@
 package cn55.controller.Validator;
 
-import java.util.HashMap;
+import java.util.regex.Pattern;
 
-public class CategoryAmountRule implements PurchaseRule {
-    @Override
-    public void validate(PurchaseValidData validData) {
-        HashMap<String, Double> categories = validData.getCategories();
+public class CategoryAmountRule implements FormRule {
 
+    private static final String VALUE_PATTERN = "\\d*";
+    private static final Pattern regex = Pattern.compile(VALUE_PATTERN);
+
+    public boolean validate(FormValidData validData) {
+        return regex.matcher(validData.getCatValueStr()).matches();
     }
 }
