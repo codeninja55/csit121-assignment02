dc015a752e55775223a3e2aedac443e8fc38aa51
Andrew Che <andrew@codeninja55.me>
2017-09-01 11:44:52 +1000

    Changed to descending order for points

--- com.cn55.program/src/cn55/model/CardModel/CardPointsComparator.java
+++ com.cn55.program/src/cn55/model/CardModel/CardPointsComparator.java
@@ -6,8 +6,8 @@ public class CardPointsComparator implements Comparator<Card> {
     @Override
     public int compare(Card c1, Card c2) {
         /* ASCENDING ORDER */
-        return Double.compare(c1.points, c2.points);
+        //return Double.compare(c1.points, c2.points);
         /* DESCENDING ORDER */
-        //return Double.compare(c2.points, c1.points);
+        return Double.compare(c2.points, c1.points);
     }
 }
